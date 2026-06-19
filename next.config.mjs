2dcc6d5715f79c080ed70778dbc3f954ca95842f
msalah-kw <msalah00@mail.com>
2026-06-19 13:41:26 +0300

    fix seo v2

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  poweredByHeader: false,
   images: {
     remotePatterns: [
       {
@@ -8,6 +9,21 @@ const nextConfig = {
         pathname: "/wp-content/uploads/**",
       },
     ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.sahbavape.com',
+          },
+        ],
+        destination: 'https://sahbavape.com/:path*',
+        permanent: true,
+      },
+    ];
   },
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ export const metadata: Metadata = {
     "أفضل متجر فيب إلكتروني في الكويت. سحبات جاهزة، بودات، نكهات سولت ونكهات فيب بأسعار منافسة وتوصيل سريع.",
   icons: {
     icon: "https://lightgrey-flamingo-522119.hostingersite.com/wp-content/uploads/2026/02/sahbavape-icon.png",
+    apple: "https://lightgrey-flamingo-522119.hostingersite.com/wp-content/uploads/2026/02/sahbavape-icon.png",
   },
 };
 
